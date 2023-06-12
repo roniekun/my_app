ad1e7ac6e64095894c7edb2e7c3d68c04094b95a
roniekun <roniebenitez01@gmail.com>
2023-06-12 20:29:38 +0800

    updated autoscroll button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React, { useState} from 'react';
-import Header from './Components/Header';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AutoscrollBotton from './components/AutoscrollButton';
 import './App.css'
-import Footer from './Components/Footer';
 
 const App = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
   const [showNavbar, setShowNavbar] = useState(window.innerWidth > 768);
+  
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setIsDesktop(screenWidth> 768);
+      setShowNavbar(screenWidth> 768);
+      setIsSmallScreen(screenWidth <= 768);
+      
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setShowNavbar,isDesktop]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (isDesktop) {
+        setShowNavbar(prevScrollPos > currentScrollPos || currentScrollPos === 0);
+        setPrevScrollPos(currentScrollPos);
+      }
+    };
+
+    if (isDesktop) { 
+      window.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (isDesktop) {
+        window.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [prevScrollPos, setShowNavbar, isDesktop]); // Add prevScrollPos and setShowNavbar as dependencies
+
+ 
   
   const handleToggleNavbar = () => {
     setShowNavbar(!showNavbar);
@@ -17,8 +62,21 @@ const App = () => {
           <button className="toggle__btn" onClick={handleToggleNavbar}>
         {showNavbar ? 'X' : '<<'}
       </button>
-      <Header showNavbar={showNavbar} setShowNavbar={setShowNavbar} handdleToggleNavbar={handleToggleNavbar} />
-      <Footer showNavbar={showNavbar} handleToggleNavbar={handleToggleNavbar} setShowNavbar={setShowNavbar} handdleToggleNavbar={handleToggleNavbar}  />
+      
+      <Header showNavbar={showNavbar} 
+              isDesktop={isDesktop}
+              isSmallScreen={isSmallScreen}
+              />
+   
+      <AutoscrollBotton 
+            isSmallScreen={isSmallScreen}
+            isDesktop={isDesktop}
+            showNavbar={showNavbar}
+      />
+      <Footer showNavbar={showNavbar}
+       handleToggleNavbar={handleToggleNavbar}
+       setShowNavbar={setShowNavbar} 
+       />
     </div> 
 
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Settings from './Settings';
+// import { Provider } from 'react-redux';
+// import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Settings/> */}
   </React.StrictMode>
 );
